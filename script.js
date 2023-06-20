2b8b1a1f9f55501ac4a3184eb8c47d749a2c89b8
Alessio Scarfone <a.scarfone@reply.it>
2023-06-20 11:29:08 +0200

    feat: update script

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.onload = function () {
     const logElement = document.getElementById('log');
     const statusElement = document.getElementById('status');
     const readBtn = document.getElementById("read");
+    let ndef = undefined;
 
     updateStatus("INIT")
     console.log("START - NDEFReader in window:", "NDEFReader" in window);
@@ -11,6 +12,21 @@ window.onload = function () {
         consoleLog("Web NFC is not supported.");
         readBtn.setAttribute("disabled", true);
     } else {
+        ndef = new NDEFReader();
+        ndef.onreading = event => {
+            const decoder = new TextDecoder();
+            for (const record of event.message.records) {
+                consoleLog("Record type:  " + record.recordType);
+                consoleLog("MIME type:    " + record.mediaType);
+                consoleLog("=== data ===\n" + decoder.decode(record.data));
+            }
+            updateStatus("onReading - END")
+        }
+        
+        ndef.onreadingerror = event => {
+            consoleLog("Error:  " + JSON.stringify(event));
+            updateStatus("onReading Error - END")
+        }
         readBtn.addEventListener('click', readTag);
     }
 
@@ -19,21 +35,11 @@ window.onload = function () {
 
     async function readTag() {
         updateStatus("Start - readTag")
-        if ("NDEFReader" in window) {
-            const ndef = new NDEFReader();
+        if ("NDEFReader" in window && ndef) {
             try {
                 updateStatus("onReading - Start")
                 await ndef.scan();
                 updateStatus("onReading - end scan")
-                ndef.onreading = event => {
-                    const decoder = new TextDecoder();
-                    for (const record of event.message.records) {
-                        consoleLog("Record type:  " + record.recordType);
-                        consoleLog("MIME type:    " + record.mediaType);
-                        consoleLog("=== data ===\n" + decoder.decode(record.data));
-                    }
-                    updateStatus("onReading - END")
-                }
             } catch (error) {
                 consoleLog(error);
             }
@@ -43,26 +49,25 @@ window.onload = function () {
         }
     }
 
-    async function writeTag() {
-        if ("NDEFReader" in window) {
-            const ndef = new NDEFReader();
-            try {
-                await ndef.write("What Web Can Do Today");
-                consoleLog("NDEF message written!");
-            } catch (error) {
-                consoleLog(error);
-            }
-        } else {
-            consoleLog("Web NFC is not supported.");
-        }
-    }
+    // async function writeTag() {
+    //     if ("NDEFReader" in window) {
+    //         const ndef = new NDEFReader();
+    //         try {
+    //             await ndef.write("What Web Can Do Today");
+    //             consoleLog("NDEF message written!");
+    //         } catch (error) {
+    //             consoleLog(error);
+    //         }
+    //     } else {
+    //         consoleLog("Web NFC is not supported.");
+    //     }
+    // }
 
     function consoleLog(data) {
         logElement.innerHTML = data + '\n';
     };
     
     function updateStatus(data) {
-        
         statusElement.innerHTML = data + '\n';
     }
 }
